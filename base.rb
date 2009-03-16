d91be89cba608a202f48b8d5fae08c69b7145497
Hector E. Gomez Morales <hectoregm@gmail.com>
2009-03-15 22:03:48 -0600

    Refactoring using convenience methods.
    Added Action Mailer initializer.

--- base.rb
+++ base.rb
@@ -18,48 +18,93 @@ rescue ScriptError => e
   Kernel::exit(1)
 end
 
+# Convenience Methods
+def cp(source, destination)
+  log 'cp', source
+  run("cp #{source} #{destination}", false)
+end
+
+def cp_r(source, destination)
+  log 'cp_r', source
+  run("cp -Rp #{get_root(source)} #{destination}", false)
+end
+
+def rm(filename)
+  log 'rm', filename
+  run("rm #{filename}", false)
+end
+
+def rmdir(dir)
+  log 'rmdir', dir
+  run("rmdir #{dir}", false)
+end
+
+def get_root(relative_destination)
+  @base_dir ||= ENV['BASE_PATH']
+  File.join(@base_dir, relative_destination)
+end
+
+def get_source(relative_destination)
+  destination = get_root(relative_destination)
+  ouput = open(destination, 'r') { |file| file.read }
+end
+
+def root_config(filename)
+  log 'config', filename
+  file(filename, get_source(filename), false)
+end
+
+def spec(filename)
+  log 'spec', filename
+  file("spec/#{filename}", get_source("spec/#{filename}"), false)
+end
+
+def model(filename)
+  log 'model', filename
+  file("app/models/#{filename}", get_source("app/models/#{filename}"), false)
+end
+
+def controller(filename)
+  log 'controller', filename
+  file("app/controllers/#{filename}", get_source("app/controllers/#{filename}"), false)
+end
+
+def view(controller, filename)
+  log 'view', filename
+  file("app/views/#{controller}/#{filename}", get_source("app/views/#{controller}/#{filename}"), false)
+end
+
+def layout(filename)
+  log 'layout', filename
+  file("app/views/layouts/#{filename}", get_source("app/views/layouts/#{filename}"), false)
+end
+
 # Get rails edge code.
 inside('vendor') do
-  run "ln -s #{edge_dir} rails"
+  log('link', 'edge rails')
+  run("ln -s #{edge_dir} rails", false)
 end
 
 ########## Git Setup ##########
 
 # Remove tmp directories
-%w[tmp/pids tmp/sessions tmp/sockets tmp/cache].each do |f|
-  run("rmdir #{f}")
+%w[tmp/pids tmp/sessions tmp/sockets tmp/cache].each do |dir|
+  rmdir dir
 end
 
 # Delete unnecessary files.
-%w[doc/README_FOR_APP public/index.html public/favicon.ico public/robots.txt].each do |f|
-  run("rm #{f}")
+%w[doc/README_FOR_APP public/index.html public/favicon.ico public/robots.txt].each do |file|
+  rm file
 end
 
 # Hold empty directories making a .gitignore file
-run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitignore \\;")
+run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitignore \\;", false)
 
 # Copy database.yml for distribution use
-run "cp config/database.yml config/database.yml.example"
+cp "config/database.yml", "config/database.yml.example"
 
 # Create  master .gitignore file
-file '.gitignore', <<-CODE
-.#*
-*~
-*#
-config/database.yml
-config/mongrel_cluster.yml
-coverage
-db/schema.rb
-db/*.sqlite3
-doc/api
-doc/app
-.DS_Store
-log/*.log
-log/*.pid
-*.swp
-tmp/**/*
-vendor/rails
-CODE
+root_config '.gitignore'
 
 # Initialize git repository
 git :init
@@ -70,16 +115,15 @@ git :init
 # plugin 'will_paginate', :git => 'git://github.com/mislav/will_paginate.git', :submodule => true
 
 # Install gems
-gem "dchelimsky-rspec", :lib => false, :env => 'test'
-gem "dchelimsky-rspec-rails", :lib => false, :env => 'test'
-gem 'sevenwire-forgery', :lib => 'forgery', :env => 'test'
-gem 'notahat-machinist', :lib => 'machinist', :env => 'test'
-gem "hectoregm-webrat", :lib => 'webrat', :env => 'test'
-gem "aslakhellesoy-cucumber", :lib => 'cucumber', :env => 'test'
+gem "rspec", :lib => false, :env => 'test'
+gem "rspec-rails", :lib => false, :env => 'test'
+gem 'sevenwire-forgery', :lib => 'forgery', :env => 'test', :source => "http://gems.github.com"
+gem 'notahat-machinist', :lib => 'machinist', :env => 'test', :source => "http://gems.github.com"
+gem 'hectoregm-webrat', :lib => 'webrat', :env => 'test', :source => "http://gems.github.com"
+gem 'aslakhellesoy-cucumber', :lib => 'cucumber', :env => 'test', :source => "http://gems.github.com"
+gem "spicycode-rcov", :lib => 'rcov', :env => 'test', :source => "http://gems.github.com"
+gem "haml"
 gem "authlogic"
-
-# Hack because of bug in template code
-gsub_file('config/environments/test.rb', /'false'/, 'false')
 
 ########## Dependecies Install ##########
 rake 'gems:install', :sudo => true
@@ -91,16 +135,22 @@ generate :rspec
 generate :forgery
 
 # Get cucumber config and task files
-run("cp #{base_dir}/cucumber.yml ./")
-run("cp #{base_dir}/lib/tasks/cucumber.rake lib/tasks")
+root_config "cucumber.yml"
+rakefile "cucumber.rake", get_source("lib/tasks/cucumber.rake")
 
 # Integrate machinist to rspec and cucumber
-run("cp #{base_dir}/spec/spec_helper.rb spec/")
-run("cp #{base_dir}/spec/blueprints.rb spec/")
+spec "spec_helper.rb"
+spec "blueprints.rb"
 append_file('features/support/env.rb', "require File.join(Rails.root, 'spec', 'blueprints')\n")
 
 # Get autotest config file.
-run "cp #{base_dir}/.autotest ./"
+root_config ".autotest"
+
+########## Application Setup ##########
+run("haml --rails .")
+
+# Initializer for action_mailer configuration
+initializer "action_mailer.rb", get_source("config/initializers/action_mailer.rb")
 
 ########## Authlogic Setup ##########
 
@@ -117,27 +167,27 @@ route "map.resource :account, :controller => \"users\""
 route "map.root :controller => \"user_sessions\", :action => \"new\""
 
 # Get models
-run "cp #{base_dir}/app/models/user.rb app/models/"
+model "user.rb"
 
 # Get controllers
-run "cp #{base_dir}/app/controllers/application_controller.rb app/controllers/"
-run "cp #{base_dir}/app/controllers/user_sessions_controller.rb app/controllers/"
-run "cp #{base_dir}/app/controllers/users_controller.rb app/controllers/"
+controller "application_controller.rb"
+controller "user_sessions_controller.rb"
+controller "users_controller.rb"
 
 # Get views
-run "cp #{base_dir}/app/views/users/new.html.erb app/views/users/"
-run "cp #{base_dir}/app/views/users/edit.html.erb app/views/users/"
-run "cp #{base_dir}/app/views/users/_form.html.erb app/views/users/"
-run "cp #{base_dir}/app/views/users/show.html.erb app/views/users/"
-run "cp #{base_dir}/app/views/user_sessions/new.html.erb app/views/user_sessions/"
+view "users", "new.html.erb"
+view "users", "edit.html.erb"
+view "users", "_form.html.erb"
+view "users", "show.html.erb"
+view "user_sessions", "new.html.erb"
 
 # Modify layouts
-run "cp #{base_dir}/app/views/layouts/application.html.erb app/views/layouts/"
-run 'rm app/views/layouts/users.html.erb'
+layout "application.html.erb"
+rm "app/views/layouts/users.html.erb"
 
 # Get authentication related cucumber features
-run "cp -Rp #{base_dir}/features/registration features/"
-run "cp -Rp #{base_dir}/features/authentication features/"
+cp_r "features/registration", "features"
+cp_r "features/authentication", "features"
 
 # Send initial commit
 git :add => "."
---
+++ base/config/initializers/action_mailer.rb
@@ -0,0 +1,31 @@
+# Email settings
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.raise_delivery_errors = false
+
+if RAILS_ENV == "production"
+  ActionMailer::Base.perform_deliveries = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address => "localhost",
+    :port => 25,
+    :domain => "domain.com"
+  }
+end
+
+if RAILS_ENV == "development"
+  ActionMailer::Base.perform_deliveries = false
+
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'domain.net',
+    :authentication => :plain,
+    :user_name => "user_name@gmail.com",
+    :password => "user_password"
+  }
+
+end
+
+if RAILS_ENV == "test"
+  ActionMailer::Base.delivery_method = :test
+end
