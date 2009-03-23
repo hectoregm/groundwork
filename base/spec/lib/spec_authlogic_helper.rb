28fa99fc45624b38181ccb163827e52218ae22f8
Hector E. Gomez Morales <hectoregm@gmail.com>
2009-03-23 12:46:57 -0600

    Refactor spec helper code.

--- base/spec/lib/spec_authlogic_helper.rb
+++ base/spec/lib/spec_authlogic_helper.rb
@@ -1,33 +1,7 @@
-module Authlogic
-  module TestHelpers
+module Authentication
+  module SpecHelpers
+
     private
-    def session_class(record) # :nodoc:
-      record.class.acts_as_authentic_config[:session_class].constantize
-    end
-
-    # Sets the session for a record. This way when you execute
-    # a request in your test, session values will be present.
-    def set_session_for(record)
-      session_class = session_class(record)
-      @request.session[session_class.session_key] = record.send(record.class.acts_as_authentic_config[:persistence_token_field])
-      @request.session["#{session_class.session_key}_id"] = record.id
-    end
-
-    # Sets the cookie for a record. This way when you execute
-    # a request in your test, cookie values will be present.
-    def set_cookie_for(record)
-      session_class = session_class(record)
-      @request.cookies[session_class.cookie_key] = record.send(record.class.acts_as_authentic_config[:persistence_token_field])
-    end
-
-    # Sets the HTTP_AUTHORIZATION header for basic HTTP auth.
-    # This way when you execute a request in your test that is trying to authenticate
-    # with HTTP basic auth, the neccessary headers will be present.
-    def set_http_auth_for(username, password)
-      session_class = session_class(record)
-      @request.env['HTTP_AUTHORIZATION'] = @controller.encode_credentials(username, password)
-    end
-
     # Creates a new confirmed user with an active session
     def log_user(attributes = {})
       user = User.make(attributes)
@@ -51,5 +25,7 @@ module Authlogic
       UserSession.stub!(:find).and_return(mock_user_session)
       mock_user_session.stub!(:user).and_return(user)
     end
+
   end
+
 end
--- base/spec/lib/spec_controller_helper.rb
+++ base/spec/lib/spec_controller_helper.rb
@@ -1,6 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-module SpecControllerHelper
+module ControllerSpecHelper
 
   def get_action
     action_name = example_group_hierarchy[-2].description_args.first.delete(':')
@@ -9,22 +7,23 @@ module SpecControllerHelper
 
   shared_examples_for "an action that requires logged out user" do
 
-    describe "requires logged out user" do
+    describe "Requires logged out user" do
+
+      before(:each) do
+        mock_log_user(mock_user)
+      end
 
       it "should not be successful with logged in user" do
-        log_user :login => "hector"
         get get_action
         response.should_not be_success
       end
 
       it "should redirect to account with logged in user" do
-        log_user :login => "hector"
         get get_action
         should redirect_to(account_url)
       end
 
       it "should have /must be logged out/ in the flash with logged in user" do
-        log_user :login => "hector"
         get get_action
         should set_the_flash(:to => /must be logged out/)
       end
@@ -35,7 +34,11 @@ module SpecControllerHelper
 
   shared_examples_for "an action that requires logged in user" do
 
-    describe "requires logged in user" do
+    describe "Requires logged in user" do
+
+      before(:each) do
+        UserSession.stub!(:find => nil)
+      end
 
       it "should not be successful with logged out user" do
         get get_action
